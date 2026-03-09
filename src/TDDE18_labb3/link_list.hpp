b6e3f7f994df874cca9d9657e3914fa053f7d617
Lucas Wihren <lucwi655@student.liu.se>
2026-03-09 10:09:16 +0100

    some problem with the insering order

--- src/TDDE18_labb3/link_list.hpp
+++ src/TDDE18_labb3/link_list.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <concepts>
+#include <sstream>
 
 // denna var ganska cool faktiskt:)
 template <typename T>
@@ -13,60 +14,103 @@ template <comparable T>
 struct node {
     node* next;
     T data;
+
+    node(T data) : data(data), next(nullptr) {}
 };
 
 template<comparable T>
 class LinkList {
 public: 
-    // inserts data in sorted order
-    template<comparable T>
-    void insert(T data) {
-        auto new_node = new node<T>;
-        auto current = first;
-
-
-
-        while (current != nullptr) {
-            if (current->next == nullptr) {
-                // då ska new node läggas in direkt som nästa
-
-                return;
-            }
-            if (current->data < data && current->next->data > data) {
-                // då ska new node in mellan current och current->next
-                
-                
-                return;
-            }    
-        }
-        // här är listan tom och new node blir first
-         
-    }
-    
-    // removes entry based on index
-    T remove(int index);
-    
-    // prints the whole list, e.g.  [1, 2, 3] 
-    void print(); 
-
-    // returns the values at index
-    T at(int index); 
-
-private:
-    node<T>* first;
 
     LinkList() {
-        first = nullptr;
+        first_ = nullptr;
+        length_ = 0;
     }
 
     ~LinkList() {
-        node<T>* current = first;
+        node<T>* current = first_;
         while (current != nullptr) {
             node<T>* next = current->next;
             delete current;
             current = next;
         }
     }
+    // inserts data in sorted order
+    void insert(T data) {
+        auto new_node = new node(data);
+        // current är alltså en pekare till nuvarande noden
+        auto current = this->first_;
+
+        while (current != nullptr) {
+            if (current->next == nullptr) {
+                // då ska new node läggas in direkt som nästa
+                current->next = new_node;
+                new_node->data = data;
+                new_node->next = nullptr;
+                length_++;
+                return;
+            }
+            else if (current->data < data && current->next->data > data) {
+                // då ska new node in mellan current och current->next
+                new_node->next = current->next;
+                current->next = new_node;
+                length_++;
+                return;
+            }
+            // flytta fram current
+            current = current->next;
+        }
+        this->first_ = new_node; 
+        length_++;
+    }
+    
+    // removes entry based on index
+    T remove(int index) {
+        return T();
+    }
+    
+    // prints the whole list, e.g.  [1, 2, 3] 
+    std::string to_string() {
+        std::ostringstream result;
+        result << "[";
+        auto current = first_;
+
+        while (current != nullptr) {
+            result << current->data;
+            
+            if (current->next != nullptr) {
+                result << ", ";
+            }
+            current = current->next;
+
+        }
+        
+        result << "]";
+        return result.str();
+    } 
+
+    // returns the values at index
+    T at(int index) {
+        auto current = first_;
+        int count = 0;
+
+        while (count < index) {
+            count++;
+            if (current == nullptr) {
+                throw std::out_of_range("Index out of range");
+            }
+            current = current->next;
+        }
+        return current->data;
+    }
+
+private:
+    node<T>* first_;
+    int length_;
+
+    
+
+    
 
     // destructor, copy constructor, move constructor,
     // copy assignment operator and move assignment operator
--- tester/test_tdde18_labb3.cpp
+++ tester/test_tdde18_labb3.cpp
@@ -1,3 +1,26 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/TDDE18_labb3/link_list.hpp"
 
+TEST_CASE("inserting") {
+    SECTION("simple insert") {
+        LinkList<int> list;
+        list.insert(5);
+        list.insert(3);
+        //list.insert(7);
+        //list.insert(7);
+        //list.insert(6);
+        //list.insert(8);
+        //list.insert(7);
+
+        std::cout << list.to_string() << std::endl;
+
+        REQUIRE(list.to_string() == "[3, 5, 6, 7, 7, 7, 8]");
+        
+        REQUIRE(list.at(0) == 3);
+        REQUIRE(list.at(1) == 5);
+        REQUIRE(list.at(2) == 7);
+        
+    };
+
+
+}
